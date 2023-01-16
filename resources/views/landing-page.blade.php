48d40d64c9498f16d554d959365f7994ee54fefb
AlexisHenry <alexis.henry150357@gmail.com>
2023-01-16 10:19:10 +0100

    fix(tagline): edit tagline (#15)

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -5,7 +5,7 @@
     <meta description="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Restiloc</title>
+    <title>Restiloc - Vehicle expertise</title>
     @vite("resources/css/app.css")
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -33,8 +33,8 @@
         <div class="wrapper">
             <div class="application__presentation">
                 <div class="container">
-                    <div class="text_main app-title">Design Your Perfect Day.</div>
-                    <div class="subtext">Get the best of health and fitness, all in one place. Download the app today !</div>
+                    <div class="text_main app-title">Restiloc is the good choice.</div>
+                    <div class="subtext">Simple and secure application for quality expertise. Download the app today !</div>
                     <a class="playstore__download" href='http://play.google.com/store/?pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
                 </div>
             </div>
