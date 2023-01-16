35ad498191cbb1eddbc2e51d945e1e6e0603e496
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-16 16:47:11 +0100

    update: routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,4 @@ Route::get('/', function () {
         "theme" => $_COOKIE['theme'] ?? "light"
     ]);
 });
+Route::get('/test', [TestController::class, 'index']);
