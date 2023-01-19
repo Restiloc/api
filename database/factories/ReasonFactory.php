e12ad8e93e66d700c3cc8cb2adfa7eaf98be74bd
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-20 00:30:12 +0100

    update: comment in factory

--- database/factories/ReasonFactory.php
+++ database/factories/ReasonFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UnavailabilitiesReasons>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reason>
  */
 class ReasonFactory extends Factory
 {
