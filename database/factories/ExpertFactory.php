4d4ef43e4c7835fbd21b024910c2faad4a99a3af
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-20 00:29:40 +0100

    update: comment in factory

--- database/factories/ExpertFactory.php
+++ database/factories/ExpertFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Experts>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Expert>
  */
 class ExpertFactory extends Factory
 {
