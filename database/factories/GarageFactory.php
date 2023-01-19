30ab40781cfd6df9af867fda0797470ffe8112fe
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-20 00:29:54 +0100

    update: comment in factory

--- database/factories/GarageFactory.php
+++ database/factories/GarageFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Garages>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Garage>
  */
 class GarageFactory extends Factory
 {
