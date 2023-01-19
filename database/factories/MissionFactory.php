f95b9501407fa87a25e1d3a6d01c07e7befafb3a
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-20 00:31:46 +0100

    add: mission factory

--- database/factories/MissionFactory.php
+++ database/factories/MissionFactory.php
@@ -2,10 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\Expert;
+use App\Models\Garage;
+use App\Models\Vehicle;
+use App\Models\Unavailability;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\MissionsExpert>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Missions>
  */
 class MissionFactory extends Factory
 {
@@ -17,7 +21,11 @@ class MissionFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'startedAt' => fake()->dateTime(),
+            'kilometersCounter' => fake()->numberBetween(0, 100000),
+            'vehicle_id' => Vehicle::all()->random()->id,
+            'expert_id' => Expert::all()->random()->id,
+            'garage_id' => Garage::all()->random()->id,
         ];
     }
 }
