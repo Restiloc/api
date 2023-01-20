b7b269f49118d346b2bb87b79f3aa07d611fa524
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-20 13:23:05 +0100

    add: test controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Expert;
+use App\Models\Folder;
+use App\Models\Garage;
+use App\Models\Mission;
+use App\Models\Reason;
+use App\Models\Unavailability;
 use App\Models\Vehicle;
+use App\Models\VehicleModel;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function index()
     {
-        dd(Vehicle::with('folder')->first());
+        // dd(Vehicle::with(['folder', 'model'])->first());
+        // dd(VehicleModel::with(['vehicles'])->first());
+        // dd(Mission::with(['folder', 'garage', 'expert', 'unavailability'])->first());
+        // dd(Expert::with(['missions'])->first());
+        // dd(Unavailability::with(['reason', 'mission'])->first());
+        // dd(Reason::with(['unavailabilities'])->first());
+        // dd(Folder::with(['vehicle', 'missions'])->first());
+        // dd(Garage::with(['missions'])->first());
     }
 }
