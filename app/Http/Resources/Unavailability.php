ab41244af283976575bb62484abcb47d2ee6100f
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-24 21:28:23 +0100

    add: api unavailability resource (#2)

---
+++ app/Http/Resources/Unavailability.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Unavailability extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'customerResponsible' => 'The customer is responsible ? ' . $this->customerResponsible,
+            'mission_id' => 'Is the mission number : ' . $this->mission_id,
+            'reason_id' => 'Is the reason number : ' . $this->reason_id
+        ];
+    }
+}
