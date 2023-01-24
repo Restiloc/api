86c6bff069c98ca72d1c045fe8f01400473a754d
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-24 21:27:53 +0100

    add: api garage resource (#2)

---
+++ app/Http/Resources/Garage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Garage extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => 'Garage : ' . $this->name,
+            'addressNumber' => 'Address number : ' . $this->addressNumber,
+            'street' => 'Street : ' . $this->street,
+            'postalCode' => 'The postal code is : ' . $this->postalCode,
+            'city' => $this->city,
+            'phoneNumber' => 'Phone number : ' . $this->phoneNumber,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+        ];
+    }
+}
