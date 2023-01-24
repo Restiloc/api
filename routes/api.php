e1db6e047b92150a9feebfb8c66b123d68bf88b2
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-24 21:31:00 +0100

    update: api routes (#2)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ExpertController;
+use App\Http\Controllers\Api\GarageController;
+use App\Http\Controllers\Api\MissionController;
+use App\Http\Controllers\Api\ReasonController;
+use App\Http\Controllers\Api\UnavailabilityController;
+use App\Http\Controllers\Api\VehicleController;
+use App\Http\Controllers\Api\VehicleModelController;
+use App\Http\Controllers\Api\VehicleExpertController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +25,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::middleware('auth:sanctum')->group(function () {
+Route::apiResource('expert', ExpertController::class);
+Route::apiResource('garage', GarageController::class);
+Route::apiResource('mission', MissionController::class);
+Route::apiResource('reason', ReasonController::class);
+Route::apiResource('unavailability', UnavailabilityController::class);
+Route::apiResource('vehicle', VehicleController::class);
+Route::apiResource('vehicle_model', VehicleModelController::class);
+Route::apiResource('vehicle_expert', VehicleExpertController::class);
+
+// });
