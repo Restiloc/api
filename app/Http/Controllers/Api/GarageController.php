165c2047f977134f5d358eaf0a05789f5ec215e8
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-24 21:23:51 +0100

    add: api garage controller

---
+++ app/Http/Controllers/Api/GarageController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Resources\Garage as ResourcesGarage;
+use App\Http\Controllers\Controller;
+use App\Models\Garage;
+use Illuminate\Http\Request;
+
+class GarageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ResourcesGarage::collection(Garage::get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if (Garage::create($request->all())) {
+            return response()->json([
+                'success' => 'Garage add with success'
+            ], 200);
+        };
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Garage  $garage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Garage $garage)
+    {
+        return new ResourcesGarage($garage);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Garage  $garage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Garage $garage)
+    {
+        if ($garage->update($request->all())) {
+            return response()->json([
+                'success' => 'Garage modify with success'
+            ], 200);
+        };
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Garage  $garage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Garage $garage)
+    {
+        if ($garage->delete()) {
+            return response()->json([
+                'success' => 'Garage delete with success'
+            ], 200);
+        };
+    }
+}
