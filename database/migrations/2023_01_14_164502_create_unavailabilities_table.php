28d13f853f2e5951395b1de190ffbd805d07d5ec
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-24 21:30:28 +0100

    update: unavailability migration

--- database/migrations/2023_01_14_164502_create_unavailabilities_table.php
+++ database/migrations/2023_01_14_164502_create_unavailabilities_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('unavailabilities', function (Blueprint $table) {
             $table->id();
-            $table->boolean("customerResponsible");
+            $table->boolean("customerResponsible")->default(false);
             $table->foreignId('reason_id')
                 ->constrained('reasons')
                 ->onDelete('no action');
             $table->foreignId('mission_id')
-                ->constrained('missions') 
+                ->constrained('missions')
                 ->onDelete('no action');
             $table->timestamps();
         });
