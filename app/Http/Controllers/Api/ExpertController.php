aab91ebf9f6bdece8bf76d0d29b255826b8f2942
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-01-24 21:23:37 +0100

    add: api expert controller

---
+++ app/Http/Controllers/Api/ExpertController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Resources\Expert as ResourcesExpert;
+use App\Http\Controllers\Controller;
+use App\Models\Expert;
+use Illuminate\Http\Request;
+
+class ExpertController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ResourcesExpert::collection(Expert::get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if (Expert::create($request->all())) {
+            return response()->json([
+                'success' => 'Expert add with success'
+            ], 200);
+        };
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Expert  $expert
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Expert $expert)
+    {
+        return new ResourcesExpert($expert);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Expert  $expert
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Expert $expert)
+    {
+        if ($expert->update($request->all())) {
+            return response()->json([
+                'success' => 'Expert modify with success'
+            ], 200);
+        };
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Expert  $expert
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Expert $expert)
+    {
+        if ($expert->delete()) {
+            return response()->json([
+               'success' => 'Expert delete with success'
+            ], 200);
+        };
+    }
+}
