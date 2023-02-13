28de6fdd8d0bb8b2c20c5c729e0f9cd0a5424096
AlexisHenry <alexis.henry150357@gmail.com>
2023-02-13 14:10:58 +0100

    feat: navigation

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -38,9 +38,10 @@ function nav() {
 				text: "Repositories",
 				collapsed: false,
 				items: [
-					{ text: "API", link: "https://github.com/Restiloc/api" },
+					{ text: "Restiloc", link: "https://github.com/Restiloc/restiloc" },
+					{ text: "REST API", link: "https://github.com/Restiloc/api" },
+					{ text: "Figma", link: "https://github.com/Restiloc/ux" },
 					{ text: "Docs", link: "https://github.com/Restiloc/docs" },
-					{ text: "UX", link: "https://github.com/Restiloc/ux" },
 				]
 			}
 
@@ -50,93 +51,19 @@ function nav() {
 function sidebarGuide() {
     return [
         {
-            text: "Repositories",
+            text: "API References",
             collapsed: false,
             items: [
-                { text: "Portfolio", link: "/guide/projects/portfolio" },
-                { text: "CDN", link: "/guide/projects/cdn" },
-                { text: "Timeline", link: "/guide/projects/timeline" },
-                { text: "Slides", link: "/guide/projects/slides" },
-                { text: "Convert.py", link: "/guide/projects/convert" },
-                {
-                    text: "URL Shortener",
-                    link: "/guide/projects/url-shortener",
-                },
-                { text: "Deployment", link: "/guide/projects/deployment" },
-                {
-                    text: "Server Maintenance",
-                    link: "/guide/projects/server-maintenance",
-                },
-                {
-                    text: "Dotfiles",
-                    link: "/guide/projects/dotfiles",
-                },
-                {
-                    text: "Archived",
-                    collapsed: false,
-                    items: [
-                        {
-                            text: "Click/s",
-                            link: "/guide/projects/archive/cpstest",
-                        },
-                        {
-                            text: "Stock Manager",
-                            link: "/guide/projects/archive/stock-manager",
-                        },
-                        {
-                            text: "Sport Addict",
-                            link: "/guide/projects/archive/sport-addict",
-                        },
-                        {
-                            text: "DOM Destroyer",
-                            link: "/guide/projects/archive/dom-destroyer",
-                        },
-                        {
-                            text: "Vanilla Portfolio",
-                            link: "/guide/projects/archive/vanilla-portfolio",
-                        },
-                        {
-                            text: "Battle Card Game",
-                            link: "/guide/projects/archive/battle-card-game",
-                        },
-                    ],
-                },
+                { text: "Authentication", link: "/guide/auth" },
+                { text: "Vehicles", link: "/guide/vehicles" },
+                { text: "Experts", link: "/guide/experts" },
+                { text: "Garages", link: "/guide/garages" },
+                { text: "Models", link: "/guide/models" },
+                { text: "Reasons", link: "/guide/reasons" },
+                { text: "Expertises", link: "/guide/expertises" },
+                { text: "Prestations", link: "/guide/prestations" },
+                { text: "Unavailabilities", link: "/guide/unavailabilities" },
             ],
-        },
-        {
-            text: "Organizations",
-            collapsed: true,
-            items: [
-                {
-                    text: "CCI-Campus",
-                    collapsed: true,
-                    items: [
-                        {
-                            text: "CCI-Appro",
-                            link: "/guide/orgs/cci-campus/cci-appro",
-                        },
-                    ],
-                },
-                {
-                    text: "Restiloc",
-                    collapsed: true,
-                    items: [
-                        { text: "Web Application", link: "/guide/orgs/restiloc/web" },
-                        {
-                            text: "Mobile Application",
-                            link: "/guide/orgs/restiloc/mobile",
-                        },
-                    ],
-                },
-            ],
-        },
-        {
-            text: "Getting Started With",
-            collapsed: true,
-            items: [
-                { text: "Sass", link: "/guide/start/sass" },
-                { text: "Symfony", link: "/guide/start/symfony" },
-            ],
-        },
+        }
     ];
 }
