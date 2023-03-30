8a22d85440ad655e9edce4c2e533dd4f92bb9c2e
Alexis Henry <91117127+AlxisHenry@users.noreply.github.com>
2023-03-30 13:40:35 +0200

    fix: remove example

--- app/Http/Controllers/Api/StatisticController.php
+++ app/Http/Controllers/Api/StatisticController.php
@@ -38,41 +38,6 @@ class StatisticController extends Controller
 
     public function getWeeklyStats()
     {
-        /**
-         * Get the number of unavailabilities of each reason, per week for each month of each existing year
-         */
-        $exampleTab = [
-            "years" => [
-                "2022" => [
-                    "weeks" => [
-                        "1" => [
-                            "reason" => [
-                                "id" => 1,
-                                "label" => "reason 1",
-                                "description" => "description 1",
-                                "color" => "#000000",
-                                "created_at" => "2021-09-01T00:00:00.000000Z",
-                                "updated_at" => "2021-09-01T00:00:00.000000Z"
-                            ],
-                            "count" => 1
-                        ],
-                        "2" => [
-                            "reason" => [
-                                "id" => 2,
-                                "label" => "reason 2",
-                                "description" => "description 2",
-                                "color" => "#000000",
-                                "created_at" => "2021-09-01T00:00:00.000000Z",
-                                "updated_at" => "2021-09-01T00:00:00.000000Z"
-                            ],
-                            "count" => 1
-                        ],
-                        // ...
-                    ]
-                ]
-            ]
-        ];
-
         $tab = [];
 
         $unavailabilities = Unavailability::all();
