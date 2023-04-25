d90eef6fc7da99f0dacf2d23fe59c84e57c880f9
Alexis Henry <91117127+AlxisHenry@users.noreply.github.com>
2023-04-25 09:35:15 +0200

    feat(store): deploy app to store (Close #9)

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -6,7 +6,7 @@
             <div class="container">
                 <div class="text_main app-title">Restiloc is the good choice.</div>
                 <div class="subtext">Simple and secure application for quality expertise. Download the app today !</div>
-                <a class="playstore__download" href='http://play.google.com/store/?pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
+                <a class="playstore__download" href='https://play.google.com/store/apps/details?id=com.restiloc'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
             </div>
         </div>
         <div class="application__illustration">
